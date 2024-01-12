b01ac8a33096b6828378b57a13eb18104ddcc7c1
coldsteelpope <coldsteelpope@gmail.com>
2024-01-12 12:49:38 +0900

    Bruteforce

---
+++ 4358/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void)
+{
+	string str;
+	cin >> str;
+
+	int startPointer = 0;
+	int endPointer = str.length() - 1;
+
+	while (startPointer < endPointer)
+	{
+		int leftPointer = startPointer;
+		int rightPointer = endPointer;
+
+		bool isPalin = true;
+
+		while (leftPointer < rightPointer)
+		{
+			if (str[leftPointer] != str[rightPointer])
+			{
+				isPalin = false;
+				break;
+			}
+			++leftPointer;
+			--rightPointer;
+		}
+		
+		if (isPalin == true)
+		{
+			break;
+		}
+		++startPointer;
+	}
+
+	cout << str.length() + startPointer << "\n";
+	return 0;
+}
