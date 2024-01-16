6042365eeea2d34dd3f1f3c68cf681d41dabaf67
coldsteelpope <coldsteelpope@gmail.com>
2024-01-16 12:54:34 +0900

    Greedy

---
+++ 1515/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace B1515
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            string str = Console.ReadLine();
+            int ptr = 0;
+
+            for (int num = 1; num <= 30000; ++num)
+            {
+                string numToString = num.ToString();
+
+                for (int numToStringIdx = 0; numToStringIdx < numToString.Length; numToStringIdx++) 
+                { 
+                    if (numToString[numToStringIdx] == str[ptr])
+                    {
+                        ++ptr;
+                    }
+
+                    if (ptr == str.Length)
+                    {
+                        Console.WriteLine(num);
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
