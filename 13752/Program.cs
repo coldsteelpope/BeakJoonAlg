bcdebe2c4daa546befe9df13639d5d783b2cbd15
coldsteelpope <coldsteelpope@gmail.com>
2024-01-24 23:35:42 +0900

    Math

---
+++ 13752/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace B13752
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int index = 0; index < n; ++index)
+            {
+                int height = int.Parse(Console.ReadLine());
+                for (int heightIdx = 0; heightIdx < height; ++heightIdx)
+                {
+                    Console.Write("=");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 14681/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace B14681
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+
+            bool[] isQuadrant = { (x > 0 && y > 0), (x < 0 && y > 0), (x < 0 && y < 0), (x > 0 && y < 0) };
+            for (int areaIdx = 0; areaIdx < isQuadrant.Length; ++areaIdx)
+            {
+                if (isQuadrant[areaIdx])
+                {
+                    Console.WriteLine(areaIdx + 1);
+                    break;
+                }
+            }
+        }
+    }
+}
