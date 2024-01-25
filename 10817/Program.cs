3df0b0c476fcb691c50f9990baf7a2d5296bc70d
coldsteelpope <coldsteelpope@gmail.com>
2024-01-25 23:56:24 +0900

    Sorting

---
+++ 10817/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace B10817
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] splitedInput = input.Split(' ');
+
+            int[] arr = new int[3];
+            for (int index = 0; index < 3; ++index)
+            {
+                arr[index] = int.Parse(splitedInput[index]);
+            }
+
+            for (int i = 0; i < 3; ++i)
+            {
+                for(int j = i + 1; j < 3; ++j)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        int temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+
+            Console.WriteLine(arr[1]);
+        }
+    }
+}
