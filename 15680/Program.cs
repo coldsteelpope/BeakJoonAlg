8f585b66c92bc3f2a7b3ed2340d51918350ef9af
coldsteelpope <coldsteelpope@gmail.com>
2024-01-28 23:15:49 +0900

    Print

---
+++ 15680/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace B15680
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            int number = int.Parse(Console.ReadLine());
+            
+            if (number == 0)
+            {
+                Console.WriteLine("YONSEI");
+            }
+            else
+            {
+                Console.WriteLine("Leading the Way to the Future");
+            }
+        }
+    }
+}
