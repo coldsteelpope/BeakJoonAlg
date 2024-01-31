cb04ae4365739371ae9359ab04e8dd762c1677e4
coldsteelpope <coldsteelpope@gmail.com>
2024-01-31 23:09:38 +0900

    구현

---
+++ 15439/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace B15439
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            Console.WriteLine(N * (N - 1));
+        }
+    }
+}
---
+++ 2753_CSharp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace B2753
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = int.Parse(Console.ReadLine());
+            
+            bool bLeapYear = true;
+            if (year % 4 != 0)
+            {
+                // 선조건: 전무
+                // 선조건이 전무인 상태에서 year % 4가 0이 아니면 평년이므로 false
+                bLeapYear = false;
+            }
+            else if (year % 100 != 0)
+            {
+                // 선조건: year % 4 == 0 는 true
+                // year % 4 == 0 이고 year != 0 이면 윤년이므로 true 
+                bLeapYear = true;
+            }
+            else if (year % 400 != 0)
+            {
+                // 선조건: year % 4 == 0 는 true && year % 100 == 0 는 true
+                // 선조건을 만족하고 year % 400가 0이 아니면, 평년이므로 false
+                bLeapYear = false;
+            }
+            else
+            {
+                // 선조건: year % 4 == 0 는 true && year % 100 == 0 는 true && year % 400 == 0 는 true
+                // 따라서 모든 조건을 만족하므로 무조건 윤년(true)!
+                bLeapYear = true;
+            }
+            
+            if (bLeapYear)
+            {
+                Console.WriteLine(1);
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+        }
+    }
+}
+
